155644bb6b55201a97009312a170df5480f1f42b
Samson Danziger <samson.danziger@gmail.com>
2016-05-13 20:16:50 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='fanfiction-api',
+      version='1.1',
+      description='Simple API to fanfiction.net',
+      download_url='https://github.com/MarsCapone/fanfiction-api',
+      url='https://marscapone.github.io/fanfiction-api',
+      author='Samson Danziger',
+      author_email='samson.danziger@gmail.com',
+      packages=['ff'],
+      install_requires=[
+          'PyYAML',
+          'pdfkit',
+          'beautifulsoup4',
+          'Ebooklib',
+          'requests'
+      ])
